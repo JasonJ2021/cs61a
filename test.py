afc719ac4fe9aef9b3a831280ca6469572af14d8
JasonJ2021 <3200104166@zju.edu.cn>
2022-07-01 19:39:16 +0800

    hw05 finished

--- test.py
+++ test.py
@@ -1,116 +1,31 @@
-from cProfile import label
-from pydoc import Helper
-
-
-def my_map(fn, seq):
-    """Applies fn onto each element in seq and returns a list.
-    >>> my_map(lambda x: x*x, [1, 2, 3])
-    [1, 4, 9]
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
     """
-    "*** YOUR CODE HERE ***"
-    return [fn(x) for x in seq]
- 
-def my_filter(pred, seq):
-    """Keeps elements in seq only if they satisfy pred.
-    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
-    [2, 4]
-    """
-    "*** YOUR CODE HERE ***"
-    return [x for x in seq if pred(x)]
-
-def my_reduce(combiner, seq):
-    """Combines elements in seq using combiner.
-    seq will have at least one element.
-    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
-    10
-    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
-    24
-    >>> my_reduce(lambda x, y: x * y, [4])
-    4
-    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
-    11
-    """
-    "*** YOUR CODE HERE ***"
-    if(len(seq) == 2):
-        return combiner(seq[0] , seq[1])
-    if(len(seq) == 1):
-        return seq[0]
-    return combiner(combiner(seq[0],seq[1]),my_reduce(combiner,seq[2:]))
-
-def count_palindromes(L):
-    """The number of palindromic words in the sequence of strings
-    L (ignoring case).
-
-    >>> count_palindromes(("Acme", "Madam", "Pivot", "Pip"))
-    2
-    """
-    return len(my_filter(helper ,L))
-
-
-def helper(s):
-    for i in range(len(s)//2):
-        if(s[i].lower() != s[len(s) - i - 1 ].lower()):
+    def helper(i):
+        if i > (n ** 0.5): # Could replace with i == n
+            return True
+        elif n % i == 0:
             return False
-    return True
+        return helper(i + 1)
+    return helper(2)
 
-def sum_tree(t):
+def primes_gen(n):
+    """Generates primes in decreasing order.
+    >>> pg = primes_gen(7)
+    >>> list(pg)
+    [7, 5, 3, 2]
     """
-    Add all elements in a tree.
-    >>> t = tree(4, [tree(2, [tree(3)]), tree(6)])
-    >>> sum_tree(t)
-    15
-    """
-    temp = label(t)
-    for b in branches(t):
-        temp += sum_tree(b)
-
-    return sum([label(t)] + [sum_tree(b) for b in branches(t)])
-
-
-
-def tree(label, branches=[]):
-	return [label] + list(branches)
-
-def label(tree):
-	return tree[0]
-
-def branches(tree):
-	return tree[1:]
-
-def is_leaf(tree):
-	return len(branches(tree)) == 0
-
-def hailstone_tree(n, h):
-    """Generates a tree of hailstone numbers that will reach N, with height H.
-    >>> print_tree(hailstone_tree(1, 0))
-    1
-    >>> print_tree(hailstone_tree(1, 4))
-    1
-        2
-            4
-                8
-                    16
-    >>> print_tree(hailstone_tree(8, 3))
-    8
-        16
-            32
-                64
-            5
-                10
-    """
-    if h == 0:
-        return tree(n)
-    branches = [hailstone_tree(2*n , h - 1)]
-    if (n  - 1 ) % 3  == 0 and ((n - 1 ) // 3 ) % 2  :
-        branches += [hailstone_tree( (n - 1 ) // 3, h - 1 )]
-    return tree(n, branches)
-
-def print_tree(t):
-    def helper(i, t):
-        print("    " * i + str(label(t)))
-        for b in branches(t):
-            helper(i + 1, b)
-    helper(0, t)
+    if n == 1:
+        return 1
+    if is_prime(n):
+        yield n
+    yield from primes_gen(n - 1)
 
 
 if __name__ == "__main__":
